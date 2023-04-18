f4405e286c55b6038f271305005b678783f4eb3f
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-04-18 10:15:47 +0800

    added getters/setters and other ide help

---
+++ src/main/java/sg/edu/nus/iss/Bicycle.java
@@ -0,0 +1,73 @@
+package sg.edu.nus.iss;
+
+public class Bicycle {
+    private int gear;
+    private int speed;
+    
+    public Bicycle() {
+
+
+    }
+
+
+    public int getGear() {
+        return gear;
+    }
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    
+    public void applyBrake(int decreaseValue) {
+        speed = speed - decreaseValue;
+
+        // speed -= decreaseValue;
+    }
+
+    public void speedUp(int increaseValue) {
+        speed += increaseValue;
+
+        // speed = speed + increaseValue;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + gear;
+        result = prime * result + speed;
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bicycle other = (Bicycle) obj;
+        if (gear != other.gear)
+            return false;
+        if (speed != other.speed)
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Bicycle [gear=" + gear + ", speed=" + speed + "]";
+    }
+
+    
+
+}
